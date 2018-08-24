e3b5d672fd027fe7e012bbea41b885e4305df3ae
Chenyang Wang <colin233@CHENYANGDEMBP.gateway>
2018-08-24 23:01:42 +1000

    add posts routes

--- models/Post.js
+++ models/Post.js
@@ -12,6 +12,45 @@ const PostSchema = new Schema({
     required: true
   },
   name: {
-    
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: "users"
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
   }
 });
+
+module.exports = mongoose.model("post", PostSchema);
