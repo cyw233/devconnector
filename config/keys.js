a58c8b6dfb7554b0f20ab1cac607eb2bae2b8bb9
Chenyang Wang <colin233@ChenyangdeMacBook-Pro.local>
2018-08-26 21:59:48 +1000

    prepare to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,8 @@
-module.exports = {
-  mongoURL: 'mongodb://colin:WANGcy888@ds129762.mlab.com:29762/dev_connector',
-  secretOrKey: 'secret'
-};
+// If we deploy this website to Heroku, "process.env.NODE_ENV" will be 
+// "production", and if it is on the local machine dev environment, the 
+// "process.env.NODE_ENV" will be different.
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // DB config
-const db = require('./config/keys').mongoURL;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
@@ -31,6 +32,16 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(___dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 
